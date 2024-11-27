b68476d45fc7d19c82c8e03dea09b9005d1de38a
vasu996 <vasubalagam999@gmail.com>
2024-11-27 22:46:13 +0530

    changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LifecycleDemoComponent } from './lifecycle-demo/lifecycle-demo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LifecycleDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
